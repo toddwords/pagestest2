8524402c79773c8570900ffca366294c7f5b30a9
Todd Anderson <toddwords@gmail.com>
2016-05-17 23:59:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var counter = 0;
 
-document.getElementByTagName("H6").addEventListener('click', function(){
+document.getElementByTagName("H6")[0].addEventListener('click', function(){
   counter++;
   document.getElementByTagName("H6")[0].innerHTML = counter.toString()
 })
